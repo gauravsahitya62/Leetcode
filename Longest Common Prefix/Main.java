ce5ee9b883b7004a674df37101f045e1631f3dea
gauravsahitya62 <gauravsahitya62@gmail.com>
2025-05-24 14:24:16 +0530

    Longest Common Prefix

---
+++ Longest Common Prefix/Main.java
@@ -0,0 +1,11 @@
+// Main.java
+public class Main {
+    public static void main(String[] args) {
+
+        LongestCommonPrefix string = new LongestCommonPrefix();
+
+
+        String[] input = {"ab", "a"};
+        System.out.println(string.longestCommonPrefix(input));
+    }
+}
