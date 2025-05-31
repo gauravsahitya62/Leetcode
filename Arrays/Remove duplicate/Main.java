a0003e7d001a7938292ffd6ca7c9aef2ee3e52b0
gauravsahitya62 <gauravsahitya62@gmail.com>
2025-05-31 14:07:39 +0530

    removed folder

--- Arrays/Remove duplicate/Main.java
+++
@@ -1,9 +0,0 @@
-// Main.java
-public class Main {
-    public static void main(String[] args) {
-        RemoveDuplicate removeDuplicate = new RemoveDuplicate();
-        
-        int numbers[] = {1,2,3,3,3,7,7,7};
-        System.out.println(removeDuplicate.removeDuplicate(numbers));
-    }
-}
